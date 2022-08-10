41f28051c2efe0b379dc1468f3a641975dd6f125
lrondon-prog <feliperondon123@hotmail.com>
2022-08-10 18:36:30 -0300

    Create service get all and factory

--- src/entities/author.ts
+++ src/entities/author.ts
@@ -1,11 +1,17 @@
 export class Author {
   constructor(
-
     public name: string,
-    public qtdBooks: number,
+    public qtdBooks = 0,
     public id?: string
+  ) {}
 
-  ) { }
+  public validate() {
+    this.validateName();
+  }
 
-
+  private validateName(){
+    if (!this.name || this.name.trim().length == 0){
+      throw new Error('autor é obrigatório e não pode ser vazio');
+    }
+  }
 }
